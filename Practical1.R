04f8532ed95bbbfb3ad7104e7fcc54bf1bfa3c95
SHRAVAN RAUT <shravan.raut23@pcu.edu.in>
2025-10-01 09:00:39 +0530

    Add files via upload

---
+++ Practical1.R
@@ -0,0 +1,15 @@
+#display first 15 rows of mt car data set
+data("mtcars")
+head(mtcars,15)
+
+#find maximum and minimum value of sepal.length from iris
+max(iris$Sepal.Length)
+
+#calculate the mean of variable mpg in mtcars
+mean(mtcars$mpg)
+
+#display structure of airqulality Data Set
+str(airquality)
+
+#check whether number 100 is grater than 50 in R
+100>50
